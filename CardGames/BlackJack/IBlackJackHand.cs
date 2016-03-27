c225af1b5784c87ad4f09bf70f9ad5872f3da5cf
Ernst Haagsman <ernsthaagsman@gmail.com>
2016-03-27 18:22:01 +0200

    Document interfaces

--- CardGames/BlackJack/IBlackJackHand.cs
+++ CardGames/BlackJack/IBlackJackHand.cs
@@ -9,10 +9,38 @@ namespace CardGames.BlackJack
 {
     public interface IBlackJackHand : IComparable<IBlackJackHand>
     {
+        /// <summary>
+        /// Add a card to the hand
+        /// </summary>
+        /// <param name="card"></param>
         void AddCard(Card card);
+
+        /// <summary>
+        /// Returns an array of all cards currently in the hand
+        /// </summary>
+        /// <returns></returns>
         Card[] GetCards();
+
+        /// <summary>
+        /// The numeric value of all cards in the hand
+        /// </summary>
+        /// <returns></returns>
         int GetValue();
+
+        /// <summary>
+        /// Is the player still alive?
+        /// 
+        /// False if value of hand over 21
+        /// </summary>
+        /// <returns></returns>
         bool IsAlive();
+
+        /// <summary>
+        /// Does the player have blackjack?
+        /// 
+        /// Meaning, a ten and an ace
+        /// </summary>
+        /// <returns></returns>
         bool IsBlackJack();
     }
 }
--- CardGames/BlackJack/IPlayer.cs
+++ CardGames/BlackJack/IPlayer.cs
@@ -9,13 +9,47 @@ namespace CardGames.BlackJack
 {
     public interface IPlayer
     {
+        /// <summary>
+        /// This is fired when a player hits, and receives a new card
+        /// </summary>
         event EventHandler<Card> onReceivedCard;
+
+        /// <summary>
+        /// This is fired when a player goes over 21
+        /// </summary>
         event Action<IPlayer> onPlayerDied;
+
+        /// <summary>
+        /// The player's hand
+        /// </summary>
         IBlackJackHand Hand { get; }
+
+        /// <summary>
+        /// The player's name
+        /// </summary>
         string Name { get; set; }
+
+        /// <summary>
+        /// Is the player over 21?
+        /// </summary>
         bool Alive { get; }
+
+        /// <summary>
+        /// When it is the player's turn, call this to stand
+        /// </summary>
         void Stand();
+
+        /// <summary>
+        /// When it is the player's turn, call this to hit
+        /// </summary>
+        /// <returns></returns>
         Card Hit();
+
+        /// <summary>
+        /// Is the player's turn over?
+        /// 
+        /// Either the player died, or he stood
+        /// </summary>
         bool Done { get; }
     }
 }
